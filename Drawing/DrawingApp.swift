1f1281372f3f2188898edda862b50af360554bb1
kadekedwin <kadekedwin12@gmail.com>
2023-11-12 15:03:41 +0800

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,20 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Kadek Edwin on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

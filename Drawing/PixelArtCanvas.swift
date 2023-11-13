03d83be7c453e2f8ff10e8381ecf7b509b985a1a
kadekedwin <kadekedwin12@gmail.com>
2023-11-13 16:12:11 +0800

    add gesture to canvas instead of the rectangle itself in pixelartcanvas file.  use a zstack to override the rectangle, so the canvas is trigered when dragged.

--- Drawing/PixelArtCanvas.swift
+++ Drawing/PixelArtCanvas.swift
@@ -9,39 +9,66 @@ import SwiftUI
 
 struct PixelArtCanvas: View {
     @State private var pixels: [[Color]] = Array(repeating: Array(repeating: .yellow, count: 16), count: 16)
+    private var resolution: CGFloat = 15
+    private var pixelSize: CGFloat = 20
+    
+    private var canvasSize: CGFloat = 15 * 20
 
     var body: some View {
         VStack {
             // Pixel Art Canvas
-            VStack(spacing: 0) {
-                ForEach(0...15, id: \.self) { row in
-                    HStack(spacing: 0) {
-                        ForEach(0...15, id: \.self) { col in
-                            Rectangle()
-                                .fill(pixels[row][col])
-                                .frame(width: 20, height: 20)
-                                .onTapGesture { location in
-                                    pixels[row][col] = .black
-                                }
-                                .gesture(
-                                    DragGesture()
-                                        .onChanged { value in
-//                                            let x = value.location.x < 0 ? Int(value.location.x / 20) - 1 : Int(value.location.x / 20)
-//                                            let y = value.location.y < 0 ? Int(value.location.y / 20) - 1 : Int(value.location.y / 20)
-                                            
-                                            let x = Int(value.location.x / 20)
-                                            let y = Int(value.location.y / 20)
-                                            
-                                            print(x)
-                                            print(y)
-                                            
-                                            if(row + y <= 15 && col + x <= 15 && row + y >= 0 && col + x >= 0) {
-                                                pixels[row + y][col + x] = .black
-                                            }
-                                        }
-                                )
+            ZStack {
+                VStack(spacing: 0) {
+                    ForEach(0..<Int(resolution), id: \.self) { row in
+                        HStack(spacing: 0) {
+                            ForEach(0..<Int(resolution), id: \.self) { col in
+                                Rectangle()
+                                    .fill(pixels[row][col])
+                                    .frame(width: pixelSize, height: pixelSize)
+//                                    .onTapGesture { location in
+//                                        pixels[row][col] = .black
+//                                    }
+//                                    .gesture(
+//                                        DragGesture()
+//                                            .onChanged { value in
+////                                                let x = value.location.x < 0 ? Int(value.location.x / 20) - 1 : Int(value.location.x / 20)
+////                                                let y = value.location.y < 0 ? Int(value.location.y / 20) - 1 : Int(value.location.y / 20)
+//                                                
+//                                                let x = Int(value.location.x / 20)
+//                                                let y = Int(value.location.y / 20)
+//                                                
+//                                                print(x)
+//                                                print(y)
+//                                                
+//                                                if(row + y <= 15 && col + x <= 15 && row + y >= 0 && col + x >= 0) {
+//                                                    pixels[row + y][col + x] = .black
+//                                                }
+//                                            }
+//                                    )
+                                
+                            }
                         }
                     }
+                }
+                
+                Canvas { context, size in
+                    
+                }
+                .frame(width: canvasSize, height: canvasSize)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            let x = Int(value.location.x / 20)
+                            let y = Int(value.location.y / 20)
+                            
+                            pixels[y][x] = .black
+                        }
+                )
+                .onTapGesture { location in
+                    let x = Int(location.x / 20)
+                    let y = Int(location.y / 20)
+                    
+                    pixels[y][x] = .black
                 }
             }
 
